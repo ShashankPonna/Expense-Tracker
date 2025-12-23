5c4cc73c940ecbd4437186eef67d3a2e4bb6cfb9
ShashankPonna <ponnashashank2806@gmail.com>
2025-12-23 23:06:40 +0530

    Changed Nav Bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,7 +13,7 @@ const Nav = () => {
         <div className="links hidden sm:flex justify-between gap-10 items-center text-1xl lg:text-2xl font-semibold text-blue-800">
           <Link to="/AddExpense">Add Expense</Link>
           <Link to="/Shared">Shared Expense</Link>
-          <Link to="/AllExpense">All Expense</Link>
+          {/* <Link to="/AllExpense">All Expense</Link> */}
         </div>
         <div className="sm:hidden flex justify-center items-center">
           <button id="menu-btn" className="text-2xl " onClick={() => {setham(!ham)}}>
@@ -24,7 +24,7 @@ const Nav = () => {
         <div className={`links flex flex-col w-full bg-blue-300 justify-center  items-center text-2xl lg:text-2xl font-semibold text-blue-800 ${ham ? "flex" : "hidden"}`}>
           <Link to="/AddExpense" className="p-2 border-t-2 w-full text-center">Add Expense</Link>
           <Link to="/Shared" className="p-2 border-t-2 w-full text-center">Shared Expense</Link>
-          <Link to="/AllExpense" className="p-2 border-2 w-full text-center">All Expense</Link>
+          {/* <Link to="/AllExpense" className="p-2 border-2 w-full text-center">All Expense</Link> */}
         </div>
     </>
   );
